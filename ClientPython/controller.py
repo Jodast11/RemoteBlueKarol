aa5b04cbc498ffcb432b98d701e2d91c5e44ec94
Jodast11 <cometome103@gmail.com>
2022-01-02 15:05:45 +0100

    added required jars

--- ClientPython/controller.py
+++ ClientPython/controller.py
@@ -2,14 +2,4 @@ import robot as r
 
 r.createRobot()
 
-r.VerzoegerungSetzen(1)
-
-while True:
-    if r.IstWand():
-        r.RechtsDrehen()
-    else:
-        r.Hinlegen()
-        r.Schritt()
-
-
 r.waitForCommands()
